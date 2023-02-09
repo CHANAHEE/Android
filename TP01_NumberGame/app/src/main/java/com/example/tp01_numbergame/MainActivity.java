b4b9d8dfb39030c1d6c1330078030c63b5c8b008
CHANAHEE <tjdrjs0803@naver.com>
2023-02-09 17:44:59 +0900

    23/02/09 17:44 -- TP > numberBaseball

---
+++ TP01_NumberGame/app/src/main/java/com/example/tp01_numbergame/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.tp01_numbergame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
